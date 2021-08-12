307e89ed6cc03aedaee88108e427960f29fb9d43
Brian Smith <brian.bubbam@gmail.com>
2021-08-12 17:26:55 -0500

    commit tree ea209989fd81c6c0eff71c44abac83783e068bc7

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,7 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # added rescue_from
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   # GET /birds
   def index
@@ -11,6 +13,9 @@ class BirdsController < ApplicationController
   def create
     bird = Bird.create(bird_params)
     render json: bird, status: :created
+    # we do not need it here because we have it up top in our code
+  # rescue ActiveRecord::RecordInvalid => invalid
+  #   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   # GET /birds/:id
@@ -24,6 +29,9 @@ class BirdsController < ApplicationController
     bird = find_bird
     bird.update(bird_params)
     render json: bird
+    # we do not need it here because we have it up top in our code
+  # rescue ActiveRecord::RecordInvalid => invalid
+  #   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   # DELETE /birds/:id
@@ -34,6 +42,23 @@ class BirdsController < ApplicationController
   end
 
   private
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    # this json response will send back this
+    #     {
+    #       "errors": {
+    #       "name": ["can't be blank"],
+    #       "species": ["must be unique"]
+    #       }
+    #     } 
+
+    # we could also use full_messages to send back a slightly different message in json
+    # render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity 
+    # {
+    #  "errors": ["Name can't be blank", "Species must be unique"]
+    # } 
+  end
 
   def find_bird
     Bird.find(params[:id])
